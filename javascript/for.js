d2016d0605c6ba6954a80691c2f4a31d9508746b
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-29 18:26:47 -0500

    Ciclo for en js

---
+++ javascript/for.js
@@ -0,0 +1,9 @@
+// ciclo for (para)
+// inicio
+// condición para terminar
+// actualización del valor del contador
+for (var contador=1; contador <= 5; contador++) {
+    // los pasos que la consola necesita hacer si la
+    // condición esté satisfecha
+    console.log(contador)
+}
