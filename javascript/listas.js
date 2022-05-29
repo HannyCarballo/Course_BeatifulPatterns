c1f156e52d0f05655fcb96d8e611a31bf496d89b
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-29 18:15:12 -0500

    Listas en js

---
+++ javascript/listas.js
@@ -0,0 +1,6 @@
+//crear listas
+var lista_letras=[] //lista vacia
+var lista_letras=['a', 'b', 'c', 'd'] //texto
+var lista_numeros=[1,2,3,4,5] //numeros
+var lista_mixta=['a', 1, 'b', 2, 'c', 3]
+var lista_compras = ['platanos',2,'papas',5]
