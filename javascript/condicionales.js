5a4c1c5cf2c948e20e11c7958803323291767880
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-29 18:17:56 -0500

    Frases condicionales en js

---
+++ javascript/condicionales.js
@@ -0,0 +1,13 @@
+//frases condicionales
+2 > 3//más que
+2 < 3//menos que
+2 == 3//igual que
+(5-2) <= (3-2)//menos o igual que (combinación)
+(5-2) >= (3-2)//más o igual que (combinación)
+10 <= 11
+10 <= 10
+(10==10) && (10<11) //menos que e igual que
+(10 > 11) && (3 >= 5) 
+(10 == 10) && (3 >= 5)
+(10 >= 11) || ("a" == "a") // o
+(10 >= 11) || ("a" == "b") // o
