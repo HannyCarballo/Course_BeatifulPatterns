5b2a19bdecb767bac0e14d406f034da0b963c972
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-29 18:25:16 -0500

    Ciclo while en js

---
+++ javascript/while.js
@@ -0,0 +1,24 @@
+// ciclo while (mientras)
+var referencia = 2022
+var este_ano = 2022
+while (referencia == este_ano) {
+    // los pasos que la consola necesita hacer si 
+    // la condición estén satisfecha
+    console.log("El año en que estoy es la referencia")
+}
+
+// ciclo while (mientras) Parte 2
+
+// imprimir numeros entre 1 y 5
+// punto de inicio: 1
+// punto de termino: 5
+// aumentando los numeros por 1
+// resultado deseado: 1, 2, 3, 4, 5
+
+var contador = 1;
+while (contador < 6) {
+    // los pasos que la consola necesita hacer si 
+    // la condición esté satisfecha
+    contador = contador + 1
+    console.log(contador)
+}
