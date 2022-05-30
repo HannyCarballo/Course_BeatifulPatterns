e2c6856e5c85990a2547dc85e6cff8e4beebcecb
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-29 20:15:10 -0500

    Funciones parte 2. Replicando la figura pero esta vez con js

---
+++ javascript/funciones parte 2/replicandoCirculos.js
@@ -0,0 +1,104 @@
+//circulo azul
+circulo = crear();
+color(circulo,9, 27, 224);
+talle(circulo,200,200);
+mover(circulo,50,50);
+repetirMover(circulo,0,10,2);
+repetirMover(circulo,0,-30,2);
+repetirMover(circulo,0,-30,2);
+repetirMover(circulo,0,-20,2);
+repetirMover(circulo,20,0,2);
+
+//circulo naranja
+circulo1 = crear();
+color(circulo1,237, 172, 31);
+talle(circulo1,200,200);
+mover(circulo1,50,50);
+repetirMover(circulo1,-10,0,2);
+repetirMover(circulo1,-60,0,2);
+repetirMover(circulo1,-80,0,2);
+repetirMover(circulo1,-40,0,2);
+repetirMover(circulo1,0,10,2);
+repetirMover(circulo1,-20,0,2);
+repetirMover(circulo1,0,20,2);
+repetirMover(circulo1,20,0,2);
+zIndex(circulo1,5);
+
+//circulo rosa
+circulo2= crear();
+color(circulo2,245, 39, 193);
+talle(circulo2,200,200);
+mover(circulo2,50,50);
+repetirMover(circulo2,-90,-80,2);
+repetirMover(circulo2,10,10,2);
+repetirMover(circulo2,5,0,2);
+repetirMover(circulo2,5,0,2);
+repetirMover(circulo2,5,0,2);
+repetirMover(circulo2,-3,0,2);
+repetirMover(circulo2,-1,0,2);
+repetirMover(circulo2,20,0,2);
+zIndex(circulo2,5);
+
+//circulo negro
+circulo3 = crear();
+color(circulo3,10, 10, 10);
+talle(circulo3,200,200);
+mover(circulo3,50,50);
+repetirMover(circulo3,20,40,2);
+repetirMover(circulo3,-80,0,2);
+repetirMover(circulo3,-100,0,2);
+repetirMover(circulo3,-50,0,2);
+repetirMover(circulo3,-40,0,2);
+repetirMover(circulo3,0,10,2);
+repetirMover(circulo3,0,25,2);
+repetirMover(circulo3,0,-50,2);
+
+//circulo gris fuerte
+circulo4 = crear();
+color(circulo4,82, 80, 80);
+talle(circulo4,200,200);
+mover(circulo4,50,50);
+repetirMover(circulo1,-150,-20,2);
+repetirMover(circulo1,150,20,2);
+repetirMover(circulo4,-150,-20,2);
+repetirMover(circulo4,0,-30,2);
+repetirMover(circulo4,0,-30,2);
+
+//circulo gris claro
+circulo5 = crear();
+color(circulo5,179, 175, 175);
+talle(circulo5,200,200);
+mover(circulo5,50,50);
+repetirMover(circulo5,-100,30,2);
+repetirMover(circulo5,-50,30,2);
+repetirMover(circulo5,-30,30,2);
+repetirMover(circulo5,10,-5,2);
+repetirMover(circulo5,15,3,2);
+repetirMover(circulo5,-3,1,2);
+repetirMover(circulo5,20,0,2);
+
+//circulo rojo
+circulo6 = crear();
+color(circulo6,255,0,0);
+talle(circulo6,200,200);
+mover(circulo6,50,50);
+repetirMover(circulo6,-20,0,2);
+repetirMover(circulo6,-20,0,2);
+repetirMover(circulo6,-20,0,2);
+repetirMover(circulo6,-20,0,2);
+repetirMover(circulo6,0,20,2);
+repetirMover(circulo6,20,0,2);
+zIndex(circulo6,5);
+
+//circulo verde
+circulo7 = crear();
+color(circulo7,5, 87, 7);
+talle(circulo7,200,200);
+mover(circulo7,50,50);
+repetirMover(circulo7,0,-30,2);
+repetirMover(circulo7,0,60,2);
+repetirMover(circulo7,0,60,2);
+repetirMover(circulo7,20,0,2);
+repetirMover(circulo7,30,0,2);
+repetirMover(circulo7,-5,2,2);
+repetirMover(circulo7,20,0,2);
